5ef54577a1fe831f75ac818ecf7571ea476fa1b3
Matthew Herrera <mherrera@kaipartners.com>
2024-04-18 10:58:06 -0700

    routing fix for rag and workspaces

--- lib/user-interface/react-app/src/app.tsx
+++ lib/user-interface/react-app/src/app.tsx
@@ -15,6 +15,16 @@ import "./styles/app.scss";
 import Embedded from "./pages/chatbot/embedded/embedded.tsx";
 import TestPage from "./pages/test-page.tsx";
 import WelcomeTest from "./pages/welcome_test.tsx";
+import AddData from "./pages/rag/add-data/add-data.tsx";
+import CreateWorkspace from "./pages/rag/create-workspace/create-workspace.tsx";
+import CrossEncoders from "./pages/rag/cross-encoders/cross-encoders.tsx";
+import Dashboard from "./pages/rag/dashboard/dashboard.tsx";
+import Embeddings from "./pages/rag/embeddings/embeddings.tsx";
+import Engines from "./pages/rag/engines/engines.tsx";
+import SemanticSearch from "./pages/rag/semantic-search/semantic-search.tsx";
+import RssFeed from "./pages/rag/workspace/rss-feed.tsx";
+import WorkspacePane from "./pages/rag/workspace/workspace.tsx";
+import Workspaces from "./pages/rag/workspaces/workspaces.tsx";
 // import GlobalFooter from "./components/global-footer.tsx";
 // import GlobalHeader from "./components/global-header.tsx";
 
@@ -59,19 +69,35 @@ function App() {
     >
       <Router>
           <Routes>
+            <Route index path="/" element={<TestPage />} />
             <Route path = "/test" element={<TestPage/>} />
             <Route path = "/welcome-test" element={<WelcomeTest />} />
             <Route path="/embedded" element={<Embedded />} /> 
-            <Route path="/" >
-              <Route index element={<TestPage />} />
-              <Route path="/chatbot" element={<Outlet />}>
-                <Route path="playground" element={<Playground />} />
-                <Route path="playground/:sessionId" element={<Playground />} />
-                <Route path="multichat" element={<MultiChatPlayground />} />
-                <Route path="models" element={<Models />} />
-              </Route>
-              <Route path="*" element={<NotFound />} />
+            <Route path="/chatbot" element={<Outlet />}>
+              <Route path="playground" element={<Playground />} />
+              <Route path="playground/:sessionId" element={<Playground />} />
+              <Route path="multichat" element={<MultiChatPlayground />} />
+              <Route path="models" element={<Models />} />
             </Route>
+            <Route path="/rag" element={<Outlet />}>
+              <Route path="" element={<Dashboard />} />
+              <Route path="engines" element={<Engines />} />
+              <Route path="embeddings" element={<Embeddings />} />
+              <Route path="cross-encoders" element={<CrossEncoders />} />
+              <Route path="semantic-search" element={<SemanticSearch />} />
+              <Route path="workspaces" element={<Workspaces />} />
+              <Route path="workspaces/create" element={<CreateWorkspace />} />
+              <Route
+                path="workspaces/:workspaceId"
+                element={<WorkspacePane />}
+              />
+              <Route
+                path="workspaces/:workspaceId/rss/:feedId"
+                element={<RssFeed />}
+              />
+              <Route path="workspaces/add-data" element={<AddData />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
           </Routes>
       </Router>
     </div>
