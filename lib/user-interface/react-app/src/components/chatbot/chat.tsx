f5d539ea88405ee3bee8305b56df87cca3484456
Matthew Herrera <mherrera@kaipartners.com>
2024-05-22 10:56:13 -0700

    update chatbot text

--- lib/user-interface/react-app/src/components/chatbot/chat.tsx
+++ lib/user-interface/react-app/src/components/chatbot/chat.tsx
@@ -106,7 +106,7 @@ export default function Chat(props: { sessionId?: string }) {
     const apiClient = new ApiClient(appContext);
     await apiClient.userFeedback.addUserFeedback({feedbackData});
   };
-  const collegeName = localStorage.getItem("collegeName") || "KAI Partners GenAI Chatbot"
+  const collegeName = localStorage.getItem("collegeName") || "KAI Partners GenAI"
   return (
     <div className={styles.chat_container}>
       <SpaceBetween direction="vertical" size="m">
