8eb75c9e570f82da012c8b4ef0f570aa425daaf5
Matthew Herrera <matt.herrera97@gmail.com>
2024-06-20 14:29:25 -0700

    feedback table code

--- lib/shared/layers/python-sdk/python/genai_core/user_feedback.py
+++ lib/shared/layers/python-sdk/python/genai_core/user_feedback.py
@@ -5,7 +5,7 @@ import json
 from pydantic import BaseModel
 from datetime import datetime
 
-dynamodb = boto3.resource("dynamodb")
+dynamodb_client = boto3.resource("dynamodb")
 s3_client = boto3.client("s3")
 
 USER_FEEDBACK_BUCKET_NAME = os.environ.get("USER_FEEDBACK_BUCKET_NAME")
@@ -23,7 +23,7 @@ def add_user_feedback(
     feedbackId = str(uuid.uuid4())
     timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     prefix = datetime.utcnow().strftime("user_feedback/year=%Y/month=%m/")
-    
+    sessions_table_name = os.environ["SESSIONS_TABLE_NAME"]
     item = {
         "feedbackId": feedbackId,
         "sessionId": sessionId,
@@ -35,7 +35,26 @@ def add_user_feedback(
         "feedback": feedback,
         "createdAt": timestamp
     }
+
+    new_feedback_data = {
+        "message": prompt,
+        "response": completion,
+        "feedback": feedback,
+        "date": timestamp
+    }
     
+    table = dynamodb_client.Table(table_name)
+    db_response = table.update_item(
+        Key={
+            'sessionId': session_id
+        },
+        UpdateExpression="SET feedback = :feedback",
+        ExpressionAttributeValues={
+            ':feedback': new_feedback_data
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
     response = s3_client.put_object(
         Bucket=USER_FEEDBACK_BUCKET_NAME,
         Key=f"{prefix}{feedbackId}.json",
