a69fc4717a732c07de580380948a873fad57ca66
Matthew Herrera <matt.herrera97@gmail.com>
2024-06-20 15:54:35 -0700

    feedback table fix

--- lib/user-interface/react-app/src/pages/feedback.tsx
+++ lib/user-interface/react-app/src/pages/feedback.tsx
@@ -1,21 +1,21 @@
 import BaseAppLayoutv from '../components/v2-base-app-layout';
-import { ApiClient } from "../../common/api-client/api-client";
+// import { ApiClient } from "../../common/api-client/api-client";
 
 function Feedback() {
 
-    const getSessions = useCallback(async () => {
-        if (!appContext) return;
+    // const getSessions = useCallback(async () => {
+    //     if (!appContext) return;
     
-        const apiClient = new ApiClient(appContext);
-        try {
-          const result = await apiClient.sessions.getSessions();
-          console.log(result.data!.listSessions)
-          setFeedback(result.data!.listSessions);
-        } catch (e) {
-          console.log(e);
-          setSessions([]);
-        }
-      }, [appContext]);
+    //     const apiClient = new ApiClient(appContext);
+    //     try {
+    //       const result = await apiClient.sessions.getSessions();
+    //       console.log(result.data!.listSessions)
+    //       setFeedback(result.data!.listSessions);
+    //     } catch (e) {
+    //       console.log(e);
+    //       setSessions([]);
+    //     }
+    //   }, [appContext]);
 
       
     return (
