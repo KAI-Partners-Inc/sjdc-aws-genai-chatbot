6fd191c4bd735c71d86fe892ac96748ae2b0a125
Matthew Herrera <matt.herrera97@gmail.com>
2024-06-21 03:17:42 -0700

    front end update session

--- lib/user-interface/react-app/src/pages/feedback.tsx
+++ lib/user-interface/react-app/src/pages/feedback.tsx
@@ -1,25 +1,7 @@
 import BaseAppLayoutv from '../components/v2-base-app-layout';
-import {
-    Box,
-    SpaceBetween,
-    Table,
-    Pagination,
-    Button,
-    TableProps,
-    Header,
-    CollectionPreferences,
-    Modal,
-  } from "@cloudscape-design/components";
-import { DateTime } from "luxon";
 import { React, useState, useEffect, useContext, useCallback } from "react";
-import { Link } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
-import { useCollection } from "@cloudscape-design/collection-hooks";
 import { ApiClient } from "../../common/api-client/api-client";
 import { AppContext } from "../../common/app-context";
-import RouterButton from "../wrappers/router-button";
-import { Session } from "../../API";
-
 
 type Feedback = {
     feedback: string;
@@ -33,10 +15,8 @@ function Feedback() {
     const appContext = useContext(AppContext);
     const [data, setData] =  useState<Feedback[]>([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(null)
     const getSessions = useCallback(async () => {
         if (!appContext) return;
-    
         const apiClient = new ApiClient(appContext);
         try {
           const result = await apiClient.sessions.getSessions();
@@ -52,7 +32,6 @@ function Feedback() {
           setIsLoading(false)
         } catch (e) {
           console.log(e);
-          setIsError(e);
           setIsLoading(false)
         }
       }, [appContext]);
@@ -70,10 +49,7 @@ function Feedback() {
     if (isLoading) {
         return <div>Loading...</div>;
       }
-    
-      if (isError) {
-        return <div>Error: {isError.message}</div>;
-      }
+
     return (
         <BaseAppLayoutv
             content={
@@ -102,8 +78,8 @@ return (
             </tr>
         </thead>
         <tbody>
-            {data.map((item, index) => (
-            <tr key={index}>
+            {data.map((item: { feedback: String; date: String; message: String; response: String; }) => (
+            <tr>
                 <td>{item.feedback}</td>
                 <td>{item.date}</td>
                 <td>{item.message}</td>
