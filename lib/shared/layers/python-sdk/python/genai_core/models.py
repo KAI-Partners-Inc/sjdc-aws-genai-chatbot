9ff811f549904a1393d832ce6c0b10a9c2f92a2d
Matthew Herrera <matt.herrera97@gmail.com>
2024-08-08 10:28:43 -0700

    add kaip

--- lib/shared/layers/python-sdk/python/genai_core/models.py
+++ lib/shared/layers/python-sdk/python/genai_core/models.py
@@ -52,17 +52,16 @@ def get_custom_bedrock_agent():
             "outputModalities": [Modality.TEXT.value],
             "interface": ModelInterface.LANGCHIAN.value,
             "ragSupported": True,
+        },
+        {
+            "provider": Provider.BEDROCK.value,
+            "name": "KAIP_Model",
+            "streaming": True,
+            "inputModalities": [Modality.TEXT.value],
+            "outputModalities": [Modality.TEXT.value],
+            "interface": ModelInterface.LANGCHIAN.value,
+            "ragSupported": True,
         }]
-        # },
-        # {
-        #     "provider": Provider.BEDROCK.value,
-        #     "name": "KAIP_Model",
-        #     "streaming": True,
-        #     "inputModalities": [Modality.TEXT.value],
-        #     "outputModalities": [Modality.TEXT.value],
-        #     "interface": ModelInterface.LANGCHIAN.value,
-        #     "ragSupported": True,
-        # }]
 
 
 def list_openai_models():
