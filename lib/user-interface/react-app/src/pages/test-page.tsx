c847e9ecac01d02179fcf055234a098633f947c1
Matthew Herrera <mherrera@kaipartners.com>
2025-02-07 12:09:42 -0800

    fix error with unused resources

--- lib/user-interface/react-app/src/pages/test-page.tsx
+++ lib/user-interface/react-app/src/pages/test-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/testpage.scss';
 import kaipLogo from '../assets/images/kaip_logo.png';
